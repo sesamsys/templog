5162149cf94f6e77d7492095ff4ad81a99b94910
sesam <petersz@me.com>
2020-06-04 09:54:10 +0200

    added humidity

--- rrd_graph.sh
+++ rrd_graph.sh
@@ -29,3 +29,30 @@ rrdtool graph /opt/templog/temp_weekly.png \
 --no-legend \
 DEF:temp=/opt/templog/data/templog.rrd:temperature:AVERAGE \
 LINE2:temp#ff0000:"temperature"
+
+rrdtool graph /opt/templog/hum_hourly.png \
+-w 500 -h 300 --full-size-mode -a PNG --slope-mode \
+--start -1h --end -300s \
+--vertical-label "humidity (%)" \
+--title "Last hour \n" \
+--no-legend \
+DEF:hum=/opt/templog/data/templog.rrd:humidity:AVERAGE \
+LINE2:hum#0000ff:"humidity"
+
+rrdtool graph /opt/templog/hum_daily.png \
+-w 500 -h 300 --full-size-mode -a PNG --slope-mode \
+--start -1d --end -300s \
+--vertical-label "humidity (%)" \
+--title "Last day \n" \
+--no-legend \
+DEF:hum=/opt/templog/data/templog.rrd:humidity:AVERAGE \
+LINE2:hum#0000ff:"humidity"
+
+rrdtool graph /opt/templog/hum_weekly.png \
+-w 1020 -h 300 --full-size-mode -a PNG --slope-mode \
+--start -1w --end -300s \
+--vertical-label "humidity (%)" \
+--title "Last week \n" \
+--no-legend \
+DEF:hum=/opt/templog/data/templog.rrd:humidity:AVERAGE \
+LINE2:hum#0000ff:"humidity"
