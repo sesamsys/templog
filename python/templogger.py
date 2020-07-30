4c2460577ff7de263874c525b8b2f00ced2a8d8b
sesamsys <petersz@me.com>
2020-07-30 15:18:08 +0200

    templogger.py error handling

--- python/templogger.py
+++ python/templogger.py
@@ -9,12 +9,12 @@ import adafruit_dht
 dhtDevice = adafruit_dht.DHT22(board.D4)
 
 def do_update():
-    timestamp = time.time()
-    temperature = dhtDevice.temperature
-    humidity = dhtDevice.humidity
 # in case of error, retry after 5 seconds
     for retry in (5, 1):
         try:
+            timestamp = time.time()
+            temperature = dhtDevice.temperature
+            humidity = dhtDevice.humidity
             rrdtool.update("/opt/templog/data/templog.rrd",
                            "%d:%s:%s" % (timestamp,
                                         humidity,
